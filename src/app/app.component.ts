7ee2e8f3be0c0b77971612e890d86e8f6521e1ca
Matthew-swin <103219809@swin.edu.au>
2020-11-02 20:42:34 +1100

    starting to look ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Go ahead and pick you pussy!';
+  title = 'Lets play a game';
+
+  
 }
