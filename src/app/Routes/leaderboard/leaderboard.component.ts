d29139870df740f6aefdbc3d1e488d6f338eb43e
Matthew-swin <103219809@swin.edu.au>
2020-11-04 19:26:39 +1100

    possible maybe

--- src/app/Routes/leaderboard/leaderboard.component.ts
+++ src/app/Routes/leaderboard/leaderboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { LeaderboardService } from 'src/app/services/leaderboard.service';
 import { GameService } from '../../services/game.service'
 
 @Component({
@@ -8,7 +9,7 @@ import { GameService } from '../../services/game.service'
 })
 export class LeaderboardComponent implements OnInit {
 
-  constructor(private GameService: GameService) { }
+  constructor(private GameService: GameService, public LeaderBoardService: LeaderboardService) { }
 
   ngOnInit(): void {
   }
